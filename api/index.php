e8425c98c2e3f980d4e74d060bcbdf2126874a7c
unknown <kiferon@bk.ru>
2023-03-31 16:05:07 +0500

    first commit

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']); 
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../index.php';
